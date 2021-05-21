9e88563b9607637575c0b25c8b7e4773779beeb1
Francis <84370831+francisdevcf@users.noreply.github.com>
2021-05-21 06:01:04 -0400

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
-pkg_origin=habdemofm
+pkg_origin=insightechabdemo
 pkg_version=7.0.0
 pkg_maintainer="Eric Heiser <eheiser@chef.io>"
 pkg_license=('Apache-2.0')
